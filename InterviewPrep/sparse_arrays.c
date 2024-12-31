e4e2b039806a0952d0768a7acd471ed0e064e085
Ivan Kastle <bluehorizon186@gmail.com>
2024-12-30 17:12:22 -0800

    Added a competitive input utilities tool in C, and added the exercises time_conversion and sparse_arrays in C.

---
+++ InterviewPrep/sparse_arrays.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tools/competitiveinput.h"
+
+int *matching_strings(int, char **, int, char **, int *);
+
+int main(void)
+{
+    int strings_count = parse_int(ltrim(rtrim(readline())));
+    char **strings = malloc(strings_count * sizeof(char *));
+
+    for (int i = 0; i < strings_count; i++)
+    {
+        char *strings_item = readline();
+        *(strings + i) = strings_item;
+    }
+
+    int queries_count = parse_int(ltrim(rtrim(readline())));
+    char** queries = malloc(queries_count * sizeof(char *));
+
+    for (int i = 0; i < queries_count; i++)
+    {
+        char *queries_item = readline();
+        *(queries + i) = queries_item;
+    }
+
+    int res_count;
+    int *res = matching_strings(strings_count,
+                               strings,
+                               queries_count,
+                               queries,
+                               &res_count);
+
+    for (int i = 0; i < res_count; i++)
+    {
+        printf("%d", *(res + i));
+
+        if (i != res_count - 1)
+            printf("\n");
+    }
+    printf("\n");
+
+    for (int i = 0; i < strings_count; i++)
+        free(*(strings + i));
+
+    for (int i = 0; i < queries_count; i++)
+        free(*(queries + i));
+
+    free(strings);
+    free(queries);
+    free(res);
+
+    return 0;
+}
+
+int *matching_strings(int strings_count,
+                      char **strings,
+                      int queries_count,
+                      char **queries,
+                      int *result_count)
+{
+    int *matches_count = calloc(queries_count, sizeof(int));
+
+    for (int i = 0; i < queries_count; i++)
+        *(matches_count + i) = 0;
+
+    for (int j = 0; j < queries_count; j++)
+    {
+        char *query = *(queries + j);
+
+        for (int k = 0; k < strings_count; k++)
+        {
+            char *string = *(strings + k);
+
+            if (strcmp(query, string) == 0)
+                matches_count[j]++;
+        }
+    }
+
+    *result_count = queries_count;
+    return matches_count;
+}
---
+++ InterviewPrep/tools/competitiveinput.h
@@ -0,0 +1,15 @@
+#ifndef _COMPETITIVE_PROGRAMMING_INPUT_
+#define _COMPETITIVE_PROGRAMMING_INPUT_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+char *readline();
+char *ltrim(char *);
+char *rtrim(char *);
+int parse_int(char *);
+
+#endif // _COMPETITIVE_PROGRAMMING_INPUT_
